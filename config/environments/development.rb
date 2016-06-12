57507bb3fee7a01ea4738b64fbb8f49ace83d0f6
Sulman Baig <sulman1990@hotmail.com>
2016-06-12 20:46:47 +0500

    MAILER - RESET PASSWORD

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,19 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.active_record.raise_in_transactional_callbacks = true
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => ENV["GMAIL_EMAIL"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => :login,
+    :enable_starttls_auto => true
+  }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: "sessions", registrations: "registrations"}
   get '/my_current_user' => "users#my_current_user"
   match 'users/:id' => 'users#update_user', via: [:patch]
+  get '/send_password' => "users#reset_password"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
